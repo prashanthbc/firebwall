76ffaa3d7307adebe419628f1cd914335cd087ec
nightstrike9809@gmail.com <nightstrike9809@gmail.com@006fc08c-d9df-6720-8989-b5458da613c0>
2012-02-26 21:31:35 +0000

    Updated translation in BasicFirewallControl

--- trunk/passthru/Modules/BasicFirewallModule/BasicFirewallControl.cs
+++ trunk/passthru/Modules/BasicFirewallModule/BasicFirewallControl.cs
@@ -91,6 +91,46 @@ namespace PassThru
             {
                 listBox1.Items.Add(rule);
             }
+            switch (FM.LanguageConfig.GetCurrentLanguage())
+            {
+                case FM.LanguageConfig.Language.NONE:
+                case FM.LanguageConfig.Language.ENGLISH:
+                    button1.Text = "Add Rule";
+                    button2.Text = "Remove Rule";
+                    buttonMoveDown.Text = "Move Down";
+                    buttonMoveUp.Text = "Move Up";
+                    break;
+                case FM.LanguageConfig.Language.PORTUGUESE:
+                    button1.Text = "Adicionar regra";
+                    button2.Text = "remover Regra";
+                    buttonMoveDown.Text = "mover para Baixo";
+                    buttonMoveUp.Text = "mover para cima";
+                    break;
+                case FM.LanguageConfig.Language.RUSSIAN:
+                    button1.Text = "Добавить правило";
+                    button2.Text = "Удалить правило";
+                    buttonMoveDown.Text = "спускать";
+                    buttonMoveUp.Text = "вверх";
+                    break;
+                case FM.LanguageConfig.Language.SPANISH:
+                    button1.Text = "Añadir regla";
+                    button2.Text = "Eliminar la regla";
+                    buttonMoveDown.Text = "Bajar";
+                    buttonMoveUp.Text = "Subir";
+                    break;
+                case FM.LanguageConfig.Language.CHINESE:
+                    button1.Text = "新增规则";
+                    button2.Text = "删除规则";
+                    buttonMoveDown.Text = "下移";
+                    buttonMoveUp.Text = "动起来";
+                    break;
+                case FM.LanguageConfig.Language.GERMAN:
+                    button1.Text = "Regel hinzufügen";
+                    button2.Text = "Regel entfernen";
+                    buttonMoveDown.Text = "Nach unten";
+                    buttonMoveUp.Text = "Nach oben";
+                    break;
+            }
         }
 
         private void buttonMoveDown_Click(object sender, EventArgs e)
