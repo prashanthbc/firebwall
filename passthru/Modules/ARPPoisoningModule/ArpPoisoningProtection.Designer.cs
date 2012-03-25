e67cf4b81264e04e4d5a4fccef02e38219114ab5
nightstrike9809 <nightstrike9809@gmail.com>
2012-03-25 01:06:18 +0000

    Added a Clear cache button to the ARPPP module gui

--- passthru/Modules/ARPPoisoningModule/ArpPoisoningProtection.Designer.cs
+++ passthru/Modules/ARPPoisoningModule/ArpPoisoningProtection.Designer.cs
@@ -29,15 +29,18 @@
         private void InitializeComponent()
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.checkBoxSave = new System.Windows.Forms.CheckBox();
             this.checkBoxLogPoisoning = new System.Windows.Forms.CheckBox();
             this.checkBoxLogUnsolicited = new System.Windows.Forms.CheckBox();
             this.checkBoxRectify = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // listBox1
@@ -49,24 +52,13 @@
             this.listBox1.Size = new System.Drawing.Size(403, 203);
             this.listBox1.TabIndex = 0;
             // 
-            // button1
-            // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button1.Location = new System.Drawing.Point(3, 241);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(403, 37);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Remove Entry";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.listBox1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 2);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -140,6 +132,43 @@
             this.checkBoxRectify.UseVisualStyleBackColor = true;
             this.checkBoxRectify.CheckedChanged += new System.EventHandler(this.checkBoxRectify_CheckedChanged);
             // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 241);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(403, 37);
+            this.tableLayoutPanel3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Remove Entry";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(204, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 31);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Clear Cache";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // ArpPoisoningProtection
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -151,6 +180,7 @@
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -158,12 +188,14 @@
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.CheckBox checkBoxSave;
         private System.Windows.Forms.CheckBox checkBoxLogPoisoning;
         private System.Windows.Forms.CheckBox checkBoxLogUnsolicited;
         private System.Windows.Forms.CheckBox checkBoxRectify;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- passthru/Modules/ARPPoisoningModule/ArpPoisoningProtection.cs
+++ passthru/Modules/ARPPoisoningModule/ArpPoisoningProtection.cs
@@ -48,19 +48,7 @@ namespace PassThru
 
         private void button1_Click(object sender, EventArgs e)
         {
-            try
-            {
-                if (listBox1.SelectedItem != null)
-                {
-                    string i = (string)listBox1.SelectedItem;
-                    IPAddress ip = IPAddress.Parse(i.Split(' ')[2]);
-                    cache.Remove(ip);
-                    saap.UpdateCache(cache);
-                    cache = saap.GetCache();
-                    saap_UpdatedArpCache();
-                }
-            }
-            catch { }
+
         }
 
         private void checkBoxSave_CheckedChanged(object sender, EventArgs e)
@@ -92,6 +80,7 @@ namespace PassThru
                     checkBoxLogUnsolicited.Text = "Log Unsolicited";
                     checkBoxLogPoisoning.Text = "Log Attacks";
                     button1.Text = "Remove Entry";
+                    button2.Text = "Clear Cache";
                     checkBoxRectify.Text = "Rectify Attacks";
                     break;
                 case LanguageConfig.Language.PORTUGUESE:
@@ -99,6 +88,7 @@ namespace PassThru
                     checkBoxLogUnsolicited.Text = "Entrar Unsolicited";
                     checkBoxLogPoisoning.Text = "Entrar Ataques";
                     button1.Text = "remover Entrada";
+                    button2.Text = "Limpar Cache";
                     checkBoxRectify.Text = "Eliminar os ataques";
                     break;
                 case LanguageConfig.Language.CHINESE:
@@ -106,6 +96,7 @@ namespace PassThru
                     checkBoxLogUnsolicited.Text = "登录未经请求";
                     checkBoxLogPoisoning.Text = "登录攻击";
                     button1.Text = "删除条目";
+                    button2.Text = "清除缓存";
                     checkBoxRectify.Text = "纠正攻击";
                     break;
                 case LanguageConfig.Language.GERMAN:
@@ -113,6 +104,7 @@ namespace PassThru
                     checkBoxLogUnsolicited.Text = "Log Initiativbewerbung";
                     checkBoxLogPoisoning.Text = "Log Attacks";
                     button1.Text = "Eintrag entfernen";
+                    button2.Text = "Cache löschen";
                     checkBoxRectify.Text = "beheben Sie Angriffe";
                     break;
                 case LanguageConfig.Language.RUSSIAN:
@@ -120,6 +112,7 @@ namespace PassThru
                     checkBoxLogUnsolicited.Text = "Вход Незапрошенные";
                     checkBoxLogPoisoning.Text = "Вход атак";
                     button1.Text = "Удалить Вступление";
+                    button2.Text = "Очистить кэш";
                     checkBoxRectify.Text = "Устранение атак";
                     break;
                 case LanguageConfig.Language.SPANISH:
@@ -127,6 +120,7 @@ namespace PassThru
                     checkBoxLogUnsolicited.Text = "registro no solicitados";
                     checkBoxLogPoisoning.Text = "Los ataques de registro";
                     button1.Text = "eliminar entrada";
+                    button2.Text = "Borrar la caché";
                     checkBoxRectify.Text = "rectificar los ataques";
                     break;
             }
@@ -136,6 +130,31 @@ namespace PassThru
         private void checkBoxRectify_CheckedChanged(object sender, EventArgs e)
         {
             saap.data.RectifyAttacks = checkBoxRectify.Checked;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listBox1.SelectedItem != null)
+                {
+                    string i = (string)listBox1.SelectedItem;
+                    IPAddress ip = IPAddress.Parse(i.Split(' ')[2]);
+                    cache.Remove(ip);
+                    saap.UpdateCache(cache);
+                    cache = saap.GetCache();
+                    saap_UpdatedArpCache();
+                }
+            }
+            catch { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cache.Clear();
+            saap.UpdateCache(cache);
+            cache = saap.GetCache();
+            saap_UpdatedArpCache();
         }
     }
 }
