72413286190168a82b0d7ce8646803cdd9a4c1c0
shodivine <shodivine@gmail.com>
2012-03-26 05:44:23 +0000

    * Started basic firewall rule editing

--- passthru/Modules/BasicFirewallModule/BasicFirewallControl.Designer.cs
+++ passthru/Modules/BasicFirewallModule/BasicFirewallControl.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
             this.buttonMoveUp = new System.Windows.Forms.Button();
             this.buttonMoveDown = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
             this.SuspendLayout();
@@ -63,12 +64,33 @@
             this.listBox1.Size = new System.Drawing.Size(453, 440);
             this.listBox1.TabIndex = 0;
             // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.buttonMoveDown, 0, 4);
+            this.tableLayoutPanel3.Controls.Add(this.buttonMoveUp, 0, 3);
+            this.tableLayoutPanel3.Controls.Add(this.button2, 0, 2);
+            this.tableLayoutPanel3.Controls.Add(this.editButton, 0, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(462, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 5;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 86F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(90, 440);
+            this.tableLayoutPanel3.TabIndex = 2;
+            // 
             // button2
             // 
             this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button2.Location = new System.Drawing.Point(3, 113);
+            this.button2.Location = new System.Drawing.Point(3, 179);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(84, 104);
+            this.button2.Size = new System.Drawing.Size(84, 82);
             this.button2.TabIndex = 1;
             this.button2.Text = "Remove Rule";
             this.button2.UseVisualStyleBackColor = true;
@@ -79,37 +101,18 @@
             this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button1.Location = new System.Drawing.Point(3, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(84, 104);
+            this.button1.Size = new System.Drawing.Size(84, 82);
             this.button1.TabIndex = 0;
             this.button1.Text = "Add Rule";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // tableLayoutPanel3
-            // 
-            this.tableLayoutPanel3.ColumnCount = 1;
-            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel3.Controls.Add(this.button2, 0, 1);
-            this.tableLayoutPanel3.Controls.Add(this.button1, 0, 0);
-            this.tableLayoutPanel3.Controls.Add(this.buttonMoveUp, 0, 2);
-            this.tableLayoutPanel3.Controls.Add(this.buttonMoveDown, 0, 3);
-            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel3.Location = new System.Drawing.Point(462, 3);
-            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
-            this.tableLayoutPanel3.RowCount = 4;
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(90, 440);
-            this.tableLayoutPanel3.TabIndex = 2;
-            // 
             // buttonMoveUp
             // 
             this.buttonMoveUp.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.buttonMoveUp.Location = new System.Drawing.Point(3, 223);
+            this.buttonMoveUp.Location = new System.Drawing.Point(3, 267);
             this.buttonMoveUp.Name = "buttonMoveUp";
-            this.buttonMoveUp.Size = new System.Drawing.Size(84, 104);
+            this.buttonMoveUp.Size = new System.Drawing.Size(84, 82);
             this.buttonMoveUp.TabIndex = 2;
             this.buttonMoveUp.Text = "Move up";
             this.buttonMoveUp.UseVisualStyleBackColor = true;
@@ -118,13 +121,24 @@
             // buttonMoveDown
             // 
             this.buttonMoveDown.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.buttonMoveDown.Location = new System.Drawing.Point(3, 333);
+            this.buttonMoveDown.Location = new System.Drawing.Point(3, 355);
             this.buttonMoveDown.Name = "buttonMoveDown";
-            this.buttonMoveDown.Size = new System.Drawing.Size(84, 104);
+            this.buttonMoveDown.Size = new System.Drawing.Size(84, 82);
             this.buttonMoveDown.TabIndex = 3;
             this.buttonMoveDown.Text = "Move Down";
             this.buttonMoveDown.UseVisualStyleBackColor = true;
             this.buttonMoveDown.Click += new System.EventHandler(this.buttonMoveDown_Click);
+            // 
+            // editButton
+            // 
+            this.editButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.editButton.Location = new System.Drawing.Point(3, 91);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(84, 82);
+            this.editButton.TabIndex = 4;
+            this.editButton.Text = "Edit Rule";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
             // 
             // BasicFirewallControl
             // 
@@ -149,5 +163,6 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
         private System.Windows.Forms.Button buttonMoveUp;
         private System.Windows.Forms.Button buttonMoveDown;
+        private System.Windows.Forms.Button editButton;
     }
 }
