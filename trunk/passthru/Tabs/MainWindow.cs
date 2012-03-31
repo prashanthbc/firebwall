32878694c10924adf640f70a4636f75c74e3770c
nightstrike9809@gmail.com <nightstrike9809@gmail.com@006fc08c-d9df-6720-8989-b5458da613c0>
2012-03-31 08:35:29 +0000

    OK, this GUI change actually looks somewhat nice

--- trunk/passthru/Tabs/MainWindow.cs
+++ trunk/passthru/Tabs/MainWindow.cs
@@ -19,30 +19,6 @@ namespace PassThru
 			public void Exit() {
 				Application.Exit();
 			}
-
-            /*
-             * Object handles logging of a log event to the window
-             * @param le is the log event object to be logged
-             */
-			void AddLogEvent(object le) {
-                // if the logger is busy, invoke it
-				if (textBox1.InvokeRequired)
-				{
-						System.Threading.ParameterizedThreadStart d = new System.Threading.ParameterizedThreadStart(AddLogEvent);
-						textBox1.Invoke(d, new object[] { le });
-				}
-                // else log the message
-				else
-				{
-						LogEvent e = (LogEvent)le;
-						textBox1.Text = e.time.ToString() + " " + e.Module + ": " + e.Message + "\r\n" + textBox1.Text;
-				}
-			}
-
-            // receives a log event and pushes it to AddLogEvent
-			void Instance_PushLogEvent(LogEvent e) {
-				AddLogEvent(e);
-			}
 			
             private void MainWindow_FormClosing(object sender, FormClosingEventArgs e) 
             {
@@ -55,41 +31,39 @@ namespace PassThru
 			}
 
             //RuleEditor re;
+            Tabs.LogDisplay log;
             AdapterControl ac;
             OptionsDisplay od;
             Help help;
 
 			private void MainWindow_Load(object sender, EventArgs e) 
             {
-                optionsTab.ItemSize = new Size((this.Width / 4) - 6, optionsTab.ItemSize.Height);
+                //optionsTab.ItemSize = new Size((this.Width / 4) - 6, optionsTab.ItemSize.Height);
                 System.Reflection.Assembly target = System.Reflection.Assembly.GetExecutingAssembly();
                 this.Icon = new System.Drawing.Icon(target.GetManifestResourceStream("PassThru.Resources.newIcon.ico"));
-				LogCenter.PushLogEvent += new LogCenter.NewLogEvent(Instance_PushLogEvent);
                 LogCenter.ti = new TrayIcon();
                 // call the log purger
                 LogCenter.cleanLogs();
-				//RuleEditor re = new RuleEditor();
-				//re.Dock = DockStyle.Fill;
-				//tabPage2.Controls.Add(re);
 
-                //re = new RuleEditor(null);
-                //re.Dock = DockStyle.Fill;
-                //tabPage2.Controls.Add(re);                
+                log = new Tabs.LogDisplay();
+                log.Dock = DockStyle.Fill;
+                LogCenter.PushLogEvent += new LogCenter.NewLogEvent(log.Instance_PushLogEvent);
+                splitContainer1.Panel2.Controls.Add(log);
 
                 // load up the adapter control handler
 				ac = new AdapterControl();
 				ac.Dock = DockStyle.Fill;
-				tabPage3.Controls.Add(ac);
+				//tabPage3.Controls.Add(ac);
 
                 // load up the options tab handler
                 od = new OptionsDisplay();
                 od.Dock = DockStyle.Fill;
-                tabPage2.Controls.Add(od);
+                //tabPage2.Controls.Add(od);
 
                 // load up the options tab handler
                 help = new Help();
                 help.Dock = DockStyle.Fill;
-                tabPage4.Controls.Add(help);
+                //tabPage4.Controls.Add(help);
 
                 switch (LanguageConfig.GetCurrentLanguage())
                 {
@@ -125,45 +99,39 @@ namespace PassThru
                         tabPage3.Text = "adaptadores";
                         break;
                 }
+                MainWindow_Resize(null, null);
 			}
-
-            private void optionsTab_DrawItem(object sender, DrawItemEventArgs e)
-            {
-                Font TabFont;
-                Brush BackBrush = new SolidBrush(Color.Black); //Set background color
-                Brush ForeBrush = new SolidBrush(Color.DarkRed);//Set foreground color
-                if (e.Index == this.optionsTab.SelectedIndex)
-                {
-                    TabFont = new Font(e.Font, FontStyle.Italic | FontStyle.Bold);
-                }
-                else
-                {
-                    TabFont = e.Font;
-                }
-                string TabName = this.optionsTab.TabPages[e.Index].Text;
-                StringFormat sf = new StringFormat();
-                sf.Alignment = StringAlignment.Center;
-                e.Graphics.FillRectangle(BackBrush, e.Bounds);
-                Rectangle r = e.Bounds;
-                r = new Rectangle(r.X, r.Y + 3, r.Width, r.Height - 3);
-                e.Graphics.DrawString(TabName, TabFont, ForeBrush, r, sf);
-                //Dispose objects
-                sf.Dispose();
-                if (e.Index == this.optionsTab.SelectedIndex)
-                {
-                    TabFont.Dispose();
-                    BackBrush.Dispose();
-                }
-                else
-                {
-                    BackBrush.Dispose();
-                    ForeBrush.Dispose();
-                }
-            }
 
             private void MainWindow_Resize(object sender, EventArgs e)
             {
-                optionsTab.ItemSize = new Size((this.Width / 4) - 8, optionsTab.ItemSize.Height);
+                tabPage1.Location = new Point(pictureBox1.Width / 20, (pictureBox1.Height / 2) - (tabPage1.Height / 2) - 4);
+                tabPage2.Location = new Point(12 * pictureBox1.Width / 20, (pictureBox1.Height / 2) - (tabPage2.Height / 2) - 4);
+                tabPage3.Location = new Point(5 * pictureBox1.Width / 20, (pictureBox1.Height / 2) - (tabPage3.Height / 2) - 4);
+                tabPage4.Location = new Point(16 * pictureBox1.Width / 20, (pictureBox1.Height / 2) - (tabPage4.Height / 2) - 4);                
+            }
+
+            private void tabPage1_Click(object sender, EventArgs e)
+            {
+                splitContainer1.Panel2.Controls.Clear();
+                splitContainer1.Panel2.Controls.Add(log);
+            }
+
+            private void tabPage2_Click(object sender, EventArgs e)
+            {
+                splitContainer1.Panel2.Controls.Clear();
+                splitContainer1.Panel2.Controls.Add(od);
+            }
+
+            private void tabPage2_Click_1(object sender, EventArgs e)
+            {
+                splitContainer1.Panel2.Controls.Clear();
+                splitContainer1.Panel2.Controls.Add(ac);
+            }
+
+            private void tabPage4_Click(object sender, EventArgs e)
+            {
+                splitContainer1.Panel2.Controls.Clear();
+                splitContainer1.Panel2.Controls.Add(help);
             }
 		}
 }
