6ae0557d3277d26b625b35760a911780b3addc87
nightstrike9809@gmail.com <nightstrike9809@gmail.com@006fc08c-d9df-6720-8989-b5458da613c0>
2012-05-07 05:19:09 +0000

    Version proposed for testing

--- trunk/passthru/Tabs/DownloadCenter.cs
+++ trunk/passthru/Tabs/DownloadCenter.cs
@@ -5,12 +5,15 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
 
 namespace PassThru.Tabs
 {
     public partial class DownloadCenter : Form
     {
         static DownloadCenter dc = null;
+        fireBwallMetaData meta = null;
         static object padlock = new object();
         public static DownloadCenter Instance
         {
@@ -35,16 +38,32 @@ namespace PassThru.Tabs
             else
             {
                 this.Visible = true;
+                meta = UpdateChecker.availableFirebwall;
+                this.Text = "New Version: fireBwall " + meta.version;
+                textBox1.Text = "New Version: fireBwall " + meta.version + "\r\n" + meta.Description + "\r\n\r\nChange Log:\r\n";
+                foreach (string s in meta.changelog)
+                {
+                    textBox1.Text += "\t- " + s + "\r\n";
+                }
+                ColorScheme_ThemeChanged();
             }
         }
 
         private DownloadCenter()
         {
             InitializeComponent();
+            ColorScheme.ThemeChanged += new System.Threading.ThreadStart(ColorScheme_ThemeChanged);
+        }
+
+        void ColorScheme_ThemeChanged()
+        {
+            ColorScheme.SetColorScheme(this);
         }
 
         private void DownloadCenter_Load(object sender, EventArgs e)
         {
+            System.Reflection.Assembly target = System.Reflection.Assembly.GetExecutingAssembly();
+            this.Icon = new System.Drawing.Icon(target.GetManifestResourceStream("PassThru.Resources.newIcon.ico"));
         }
 
         private void DownloadCenter_FormClosing(object sender, FormClosingEventArgs e)
@@ -53,6 +72,29 @@ namespace PassThru.Tabs
             {
                 this.Visible = false;
                 e.Cancel = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                folder = folder + Path.DirectorySeparatorChar + "firebwall";
+                if (!Directory.Exists(folder))
+                    Directory.CreateDirectory(folder);
+                folder = folder + Path.DirectorySeparatorChar + "installers";
+                if (!Directory.Exists(folder))
+                    Directory.CreateDirectory(folder);
+                string file = folder + Path.DirectorySeparatorChar + meta.filename;
+                WebClient wc = new WebClient();
+                wc.DownloadFile(meta.downloadUrl, file);
+                System.Diagnostics.Process.Start(file);
+                Program.Close(null, null);
+            }
+            catch
+            {
+                MessageBox.Show("An error occurred during downloading and installing the newest version of fireBwall.  Please visit https://firebwall.com");
             }
         }
     }
--- trunk/passthru/Tabs/OptionsDisplay.Designer.cs
+++ trunk/passthru/Tabs/OptionsDisplay.Designer.cs
@@ -42,7 +42,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.themeBox = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -99,7 +98,6 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.textBox1);
@@ -202,16 +200,6 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(134, 35);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(127, 23);
-            this.button2.TabIndex = 13;
-            this.button2.Text = "Download Center";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // OptionsDisplay
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -253,7 +241,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox themeBox;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
 
 
 
