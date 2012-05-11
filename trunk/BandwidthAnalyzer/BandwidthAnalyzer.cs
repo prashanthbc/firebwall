85118462f1da1695ab7a4f1fff488a6659949223
fourthdimension.stephen <fourthdimension.stephen@006fc08c-d9df-6720-8989-b5458da613c0>
2012-05-11 01:36:14 +0000

    Initial version of bandwidth analyzer module.  Not yet ready for prime time -- still working on additional views/analysis breakdowns and database optimizations.  At this time because of the current implementation of the system, in order for this module to work its two dependencies (SQLite.Interop.dll and System.Data.SQLite.dll) need to be placed in the installation directory because of the bug recently mentioned on trello.

---
+++ trunk/BandwidthAnalyzer/BandwidthAnalyzer.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Data.SQLite;
+using System.Collections.Generic;
+using FM;
+
+namespace BandwidthAnalyzer
+{
+    public class BandwidthAnalyzer : FirewallModule
+    {
+
+        private const string MODULE_NAME = "Bandwidth Analyzer Module";
+        private const int BUFFER_MAX_SIZE = 5000;
+        private string dbPath = null;
+
+        // flag for other classes to set for clearing the log on next write
+        public static bool DO_CLEAR_LOG = false;
+
+        public static string DB_NAME = "connection_db.db";
+
+        public const string DIRECTION_INBOUND = "0";
+        public const string DIRECTION_OUTBOUND = "1";
+
+        // string types
+        public const string TCP = "TCP";
+        public const string UDP = "UDP";
+        public const string SNMP = "SNMP";
+        public const string DHCP = "DHCP";
+        public const string DNS = "DNS";
+        public const string ARP = "ARP";
+        public const string IP = "IP";
+        public const string ICMP = "ICMP";
+        public const string ICMPV6 = "ICMPv6";
+        public const string EETH = "EEth";
+        public const string ETHERNET = "Ethernet";
+        public const string NO_IP = "N/A";
+
+        private List<String> buffer = null;
+        private int bufferSize = 0;
+
+        private SQLiteConnection db = null;
+
+        public BandwidthAnalyzer()
+            : base()
+        {
+            init();
+        }
+
+        public BandwidthAnalyzer(INetworkAdapter adapter)
+            : base(adapter)
+        {
+            init();
+        }
+
+        private void init()
+        {
+            Help();
+            initBuffer();
+            DO_CLEAR_LOG = false;
+        }
+
+        public static string GetDBPath()
+        {
+            string folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            folder = folder + Path.DirectorySeparatorChar + "firebwall";
+            if (!Directory.Exists(folder))
+                Directory.CreateDirectory(folder);
+            if (!Directory.Exists(folder))
+                Directory.CreateDirectory(folder);
+            return folder;
+        }
+
+        // create the log buffer
+        private void initBuffer()
+        {
+            dbPath = GetDBPath();
+            buffer = new List<String>();
+            bufferSize = 0;
+        }
+
+        // initialize the database connection
+        private void initDB()
+        {
+            try
+            {
+                string dbFile = GetDBPath() + Path.DirectorySeparatorChar + DB_NAME;
+                bool exists = File.Exists(dbFile);
+
+                db = new SQLiteConnection();
+                db.ConnectionString = new SQLiteConnectionStringBuilder()
+                {
+                    {"Data Source", dbFile},
+                    {"Version", "3"},
+                    {"FailIfMissing", "False"}
+                }.ConnectionString;
+                db.Open();
+
+                if (!exists)
+                {
+                    // create the table
+
+                    string createString = "drop table if exists connection_log; create table connection_log(outbound bool, timestamp int, address varchar(39), size int, protocol varchar(10))";
+
+                    SQLiteCommand command = new SQLiteCommand(createString, db);
+                    command.ExecuteNonQuery();
+
+                }
+            }
+            catch (Exception e)
+            {
+                PassThru.LogCenter.WriteErrorLog(e);
+                db = null;
+            }
+        }
+
+        // add a statement to the buffer, writing the buffer if necessary
+        private void bufferStatement(string s)
+        {
+            lock (this) {
+                if (bufferSize + 1 > BUFFER_MAX_SIZE)
+                {
+                    writeBuffer();
+                    bufferSize = 0;
+                }
+                buffer.Add(s);
+                ++bufferSize;
+            }
+        }
+
+        // write the buffer to the database and then clear it
+        private void writeBuffer()
+        {
+            lock (this)
+            {
+                try
+                {
+
+                    if (DO_CLEAR_LOG)
+                    {
+                        if (File.Exists(dbPath))
+                            File.Delete(dbPath);
+                        DO_CLEAR_LOG = false;
+                    }
+
+                    SQLiteTransaction transaction = db.BeginTransaction();
+                    SQLiteCommand command = null;
+                    foreach (String s in buffer)
+                    {
+                        command = new SQLiteCommand(s, db);
+                        command.ExecuteNonQuery();
+                    }
+                    transaction.Commit();
+                    buffer.Clear();
+                    bufferSize = 0;
+                }
+                catch (Exception e)
+                {
+                    PassThru.LogCenter.Instance.Push(MODULE_NAME, e.Message);
+                }
+            }
+        }
+
+        void Help()
+        {
+            MetaData.Name = "Bandwidth Analyzer";
+            MetaData.Version = "1.1.0.0";
+            MetaData.HelpString = "This module logs the source/destination of incoming and outgoing packets and allows for statistical and visual analysis.";
+            MetaData.Description = "Performs analysis and generates reports on bandwidth usage";
+            MetaData.Contact = "fourthidmension.stephen@gmail.com";
+            MetaData.Author = "Stephen C. (fourthdimension)";
+        }
+
+        public override ModuleError ModuleStart()
+        {
+            initDB();
+            ModuleError me = new ModuleError();
+            me.errorType = ModuleErrorType.Success;
+            return me;
+        }
+
+        public override ModuleError ModuleStop()
+        {
+            lock (this) {
+                // write any entries that may still be in the buffer to the log file
+                writeBuffer();
+
+                if (db == null)
+                {
+                    try
+                    {
+                        db.Close();
+                    }
+                    catch (Exception e)
+                    {
+                        PassThru.LogCenter.WriteErrorLog(e);
+                    }
+                }
+
+                ModuleError me = new ModuleError();
+                me.errorType = ModuleErrorType.Success;
+                return me;
+            }
+        }
+
+
+        public override System.Windows.Forms.UserControl GetControl()
+        {
+            return new ConnectionGraphControl(this) { Dock = System.Windows.Forms.DockStyle.Fill };
+        }
+
+
+
+        public override PacketMainReturn interiorMain(ref Packet in_packet)
+        {
+            // let the packet through
+            PacketMainReturn pmr = new PacketMainReturn(MODULE_NAME);
+            pmr.returnType = PacketMainReturnType.Allow;
+            try
+            {
+                // get a usable form of the packet
+                Protocol highestLayer = in_packet.GetHighestLayer();
+
+                string type = "";
+                bool outbound = false;
+                uint length = 0;
+                string ip = "";
+
+                switch (highestLayer)
+                {
+                    case Protocol.TCP:
+                        TCPPacket tcpPacket = (TCPPacket)in_packet;
+                        type = TCP;
+                        outbound = tcpPacket.Outbound;
+                        length = tcpPacket.Length();
+                        ip = outbound ? tcpPacket.DestIP.ToString() : tcpPacket.SourceIP.ToString();
+                        break;
+                    case Protocol.UDP:
+                        UDPPacket udpPacket = (UDPPacket)in_packet;
+                        type = UDP;
+                        length = udpPacket.Length();
+                        ip = outbound ? udpPacket.DestIP.ToString() : udpPacket.SourceIP.ToString();
+                        break;
+                    case Protocol.ARP:
+                        ARPPacket arpPacket = (ARPPacket)in_packet;
+                        type = ARP;
+                        outbound = arpPacket.Outbound;
+                        length = arpPacket.Length();
+                        ip = outbound ? arpPacket.ATargetIP.ToString() : arpPacket.ASenderIP.ToString();
+                        break;
+                    case Protocol.DHCP:
+                        // no packet structure for this type
+                        break;
+                    case Protocol.DNS:
+                        DNSPacket dnsPacket = (DNSPacket)in_packet;
+                        type = DNS;
+                        outbound = dnsPacket.Outbound;
+                        length = dnsPacket.Length();
+                        ip = outbound ? dnsPacket.DestIP.ToString() : dnsPacket.SourceIP.ToString();
+                        break;
+                    case Protocol.EEth:
+                        EETHPacket eethPacket = (EETHPacket)in_packet;
+                        type = EETH;
+                        outbound = eethPacket.Outbound;
+                        length = eethPacket.Length();
+                        ip = NO_IP;
+                        break;
+                    case Protocol.Ethernet:
+                        EthPacket ethPacket = (EthPacket)in_packet;
+                        type = ETHERNET;
+                        outbound = ethPacket.Outbound;
+                        length = ethPacket.Length();
+                        ip = NO_IP;
+                        break;
+                    case Protocol.ICMP:
+                        ICMPPacket icmpPacket = (ICMPPacket)in_packet;
+                        type = ICMP;
+                        outbound = icmpPacket.Outbound;
+                        length = icmpPacket.Length();
+                        ip = ip = outbound ? icmpPacket.DestIP.ToString() : icmpPacket.SourceIP.ToString();
+                        break;
+                    case Protocol.ICMPv6:
+                        ICMPv6Packet icmpv6Packet = (ICMPv6Packet)in_packet;
+                        type = ICMPV6;
+                        outbound = icmpv6Packet.Outbound;
+                        length = icmpv6Packet.Length();
+                        ip = outbound ? icmpv6Packet.DestIP.ToString() : icmpv6Packet.SourceIP.ToString();
+                        break;
+                    case Protocol.IP:
+                        IPPacket ipPacket = (IPPacket)in_packet;
+                        type = IP;
+                        outbound = ipPacket.Outbound;
+                        length = ipPacket.Length();
+                        ip = outbound ? ipPacket.DestIP.ToString() : ipPacket.SourceIP.ToString();
+                        break;
+                    case Protocol.SNMP:
+                        // no packet structure available for this type
+                        break;
+                    default:
+                        break;
+                }
+
+                if (type != "")
+                {
+
+                    bufferStatement("insert into connection_log values (" + (outbound ? DIRECTION_OUTBOUND : DIRECTION_INBOUND) + "," + 
+                        DateTime.Now.Ticks + ",'" + ip + "'," + length + ",'" + type + "')"); // using Length here...
+                                                                                // should TotalLength be used instead?
+                }
+                
+            }
+            catch (Exception e)
+            {
+                PassThru.LogCenter.Instance.Push(MODULE_NAME, e.Message);
+            }
+
+            return pmr;
+        }
+
+    }
+}
---
+++ trunk/BandwidthAnalyzer/BandwidthAnalyzerControl.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+using FM;
+
+namespace BandwidthAnalyzer
+{
+    public partial class ConnectionGraphControl : UserControl
+    {
+        public ConnectionGraphControl(BandwidthAnalyzer module)
+        {
+            InitializeComponent();
+            formatCombobox.SelectedIndex = 0;
+            updateMaxValuesTextBox();
+        }
+
+        private void entriesTickBar_ValueChanged(object sender, EventArgs e)
+        {
+            updateMaxValuesTextBox();
+        }
+
+        private void updateMaxValuesTextBox()
+        {
+            maxValuesTextBox.Text = entriesTickBar.Value + "";
+        }
+
+        private void maxValuesTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+            if (!String.IsNullOrEmpty(maxValuesTextBox.Text))
+            {
+                int maxNum = 0;
+                try
+                {
+                    maxNum = Int32.Parse(maxValuesTextBox.Text);
+                }
+                catch (Exception)
+                {
+                    string cleaned = (new Regex(@"[^\d]")).Replace(maxValuesTextBox.Text, "");
+
+                    if (!String.IsNullOrEmpty(cleaned))
+                    {
+                        maxNum = Int32.Parse(cleaned);
+                    }
+                    maxValuesTextBox.Text = cleaned;
+                }
+                entriesTickBar.Value = maxNum > entriesTickBar.Maximum ? entriesTickBar.Maximum : maxNum;
+            }
+
+        }
+
+        private string matchCheckbox(CheckBox checkbox)
+        {
+
+            string name;
+
+            if (checkbox == tcpCheckbox) {
+                name = BandwidthAnalyzer.TCP;
+            } else if (checkbox == udpCheckbox) {
+                name = BandwidthAnalyzer.UDP;
+            } else if (checkbox == eethCheckbox) {
+                name = BandwidthAnalyzer.EETH;
+            } else if (checkbox == ethernetCheckbox) {
+                name = BandwidthAnalyzer.ETHERNET;
+            } else if (checkbox == ipCheckbox) {
+                name = BandwidthAnalyzer.IP;
+            } else if (checkbox == icmpv6Checkbox) {
+                name = BandwidthAnalyzer.ICMPV6;
+            } else if (checkbox == arpCheckbox) {
+                name = BandwidthAnalyzer.ARP;
+            } else if (checkbox == icmpCheckbox) {
+                name = BandwidthAnalyzer.ICMP;
+            } else if (checkbox == dnsCheckbox) {
+                name = BandwidthAnalyzer.DNS;
+            } else if (checkbox == dhcpCheckbox) {
+                name = BandwidthAnalyzer.DHCP;
+            } else if (checkbox == snmpCheckbox) {
+                name = BandwidthAnalyzer.SNMP;
+            } else {
+                name = null;
+            }
+
+            return name;
+        }
+
+        private void generateReportButton_Click(object sender, EventArgs e)
+        {
+            // get the filepath
+            string filepath = BandwidthAnalyzer.GetDBPath() + Path.DirectorySeparatorChar +  BandwidthAnalyzer.DB_NAME;
+
+            // process the file line by line based on the selected filters
+            try
+            {
+                int packetsOut = 0;
+                int packetsIn = 0;
+                BandwidthCounter bandwidthOut = new BandwidthCounter();
+                BandwidthCounter bandwidthIn = new BandwidthCounter();
+
+                if (File.Exists(filepath))
+                {
+
+                    // get the filter parameters
+                    long startTime = startDatePicker.Value.Ticks;
+                    long endTime = endDatePicker.Value.Ticks;
+
+                    DateTime accessTime = File.GetLastAccessTime(filepath);
+
+                    try
+                    {
+                        SQLiteConnection db = new SQLiteConnection();
+                        db.ConnectionString = new SQLiteConnectionStringBuilder()
+                        {
+                            {"Data Source", BandwidthAnalyzer.GetDBPath() + Path.DirectorySeparatorChar + "connection_db.db"},
+                            {"Version", "3"},
+                            {"FailIfMissing", "False"}
+                        }.ConnectionString;
+                        db.Open();
+
+                        CheckBox[] checkboxes = new CheckBox[] { 
+                            tcpCheckbox, udpCheckbox, eethCheckbox, ethernetCheckbox, ipCheckbox,
+                            icmpv6Checkbox, arpCheckbox, icmpCheckbox, dnsCheckbox, dhcpCheckbox,
+                            snmpCheckbox
+                        };
+
+                        string protocolList = "";
+                        foreach (CheckBox checkbox in checkboxes) {
+                            if (checkbox.Checked)
+                                protocolList += ",'" + matchCheckbox(checkbox) + "'";
+                        }
+                        protocolList = protocolList.Remove(0, 1);
+
+                        string timeFilter = "";
+                        if (timestampCheckbox.Checked)
+                        {
+                            timeFilter = "timestamp between " + startTime + " and " + endTime;
+                        }
+
+                        string query = "select * from connection_log where protocol in (" + protocolList + ")" + timeFilter;
+
+                        SQLiteCommand command = new SQLiteCommand(query, db);
+
+                        using (SQLiteDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                bool outgoing = reader.GetBoolean(0);
+                                long timestamp = reader.GetInt64(1);
+                                string ip = reader.GetString(2);
+                                uint size = (uint) reader.GetInt32(3);
+                                string protocol = reader.GetString(4);
+
+                                if (outgoing)
+                                {
+                                    ++packetsOut;
+                                    bandwidthOut.AddBits(size);
+                                }
+                                else
+                                {
+                                    ++packetsIn;
+                                    bandwidthIn.AddBits(size);
+                                }
+                            }
+                        }
+                        db.Close();
+
+                        MessageBox.Show("Analysis of connections logged as of " + accessTime +
+                            "\nTotal Packets Sent: " + packetsOut +
+                            "\nTotal Packets Received: " + packetsIn +
+                            "\nTotal Outgoing Bandwidth: " + bandwidthOut.ToString() +
+                            "\nTotal Incoming Bandwidth: " + bandwidthIn.ToString());
+                    }
+                    catch (Exception ex)
+                    {
+                        PassThru.LogCenter.WriteErrorLog(ex);
+                    }
+
+                    
+
+
+
+                }
+                else
+                {
+                    MessageBox.Show("No data available.  This module needs to be enabled in " +
+                        "order for it to collect data to analyze.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            BandwidthAnalyzer.DO_CLEAR_LOG = true;
+            MessageBox.Show("Log will be cleared on next write or system exit.");
+        }
+    }
+}
